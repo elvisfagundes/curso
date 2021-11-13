456cc527f8a28a8d0aea8570e36820b470106f3e
elvisfagundes <elvis.f.lisboa.jr@hotmail.com>
2021-11-13 11:50:36 -0300

    Initial commit

---
+++ curso.api/Controllers/CursosController.cs
@@ -0,0 +1,90 @@
+﻿using curso.api.Business.Entities;
+using curso.api.Business.Repositories;
+using curso.api.Models.Cursos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace curso.api.Controllers
+{
+    [Route("api/v1/cursos")]
+    [ApiController]
+    [Authorize]
+    public class CursoController : ControllerBase
+    {
+        private readonly ICursoRepository _cursoRepository;
+        private readonly ILogger<UsuarioController> _logger;
+
+        public CursoController(ICursoRepository cursoRepository, ILogger<UsuarioController> logger)
+        {
+            _cursoRepository = cursoRepository;
+            _logger = logger;
+        }
+
+        [SwaggerResponse(statusCode: 201, description: "Sucesso ao Cadastrar um curso", Type = typeof(CursoViewModelOutput))]
+        [SwaggerResponse(statusCode: 401, description: "Não autorizado")]
+        [HttpPost]
+        [Route("")]
+        public async Task<IActionResult> Post(CursoViewModelInput cursoViewModelInput)
+        {
+            try
+            {
+                Curso curso = new Curso
+                {
+                    Nome = cursoViewModelInput.Nome,
+                    Descricao = cursoViewModelInput.Descricao
+                };
+
+                var codigoUsuario = int.Parse(User.FindFirst(c => c.Type == ClaimTypes.NameIdentifier)?.Value);
+                curso.CodigoUsuario = codigoUsuario;
+                _cursoRepository.Adicionar(curso);
+                _cursoRepository.Commit();
+
+                var cursoViewModelOutput = new CursoViewModelOutput
+                {
+                    Nome = curso.Nome,
+                    Descricao = curso.Descricao,
+                };
+
+                return Created("", cursoViewModelOutput);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.ToString());
+                return new StatusCodeResult(500);
+            }
+        }
+
+        [SwaggerResponse(statusCode: 200, description: "Sucesso ao obter os cursos", Type = typeof(CursoViewModelOutput))]
+        [SwaggerResponse(statusCode: 401, description: "Não autorizado")]
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var codigoUsuario = int.Parse(User.FindFirst(c => c.Type == ClaimTypes.NameIdentifier)?.Value);
+
+                var cursos = _cursoRepository.ObterPorUsuario(codigoUsuario)
+                    .Select(s => new CursoViewModelOutput()
+                    {
+                        Nome = s.Nome,
+                        Descricao = s.Descricao
+                    });
+
+                return Ok(cursos);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.ToString());
+                return new StatusCodeResult(500);
+            }
+        }
+    }
+}
---
+++ curso.api/Models/Usuarios/LoginViewModelOutput.cs
@@ -0,0 +1,9 @@
+﻿namespace curso.api.Models.Usuarios
+{
+    public class LoginViewModelOutput
+    {
+        public string Token { get; set; }
+
+        public UsuarioViewModelOutput Usuario { get; set; }
+    }
+}
